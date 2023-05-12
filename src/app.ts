7b5d4fc493b30e3363a3b91bcd66ec881b624c09
Pablo-Aviles-Prieto <pablo.aviles.prieto@gmail.com>
2023-05-12 12:40:50 +0200

    project initialized

---
+++ src/app.ts
@@ -0,0 +1,44 @@
+import 'dotenv/config';
+
+import http from 'http';
+import cors from 'cors';
+import express from 'express';
+import path from 'path';
+import { Server } from 'socket.io';
+
+const { PORT } = process.env;
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, '../public')));
+
+const httpServer = http.createServer(app);
+
+const io = new Server(httpServer, {
+  cors: {
+    origin: 'http://localhost:3000'
+  }
+});
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+  socket.on('chat msg', (msg: string) => {
+    // This will send the event to all connected clients, including the one that initiated the event.
+    io.emit('chat msg', msg);
+  });
+  socket.on('new line segment', (lineSegment) => {
+    // This will send the event to all clients except for the one that initiated the event
+    socket.broadcast.emit('new line segment', lineSegment);
+  });
+  socket.on('clear board', () => {
+    socket.broadcast.emit('clear board');
+  });
+});
+
+httpServer.listen(PORT, () => console.info(`Server running and listening at http://localhost:${PORT}`));
